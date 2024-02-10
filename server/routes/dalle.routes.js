5dffa7bb3f121a9d424970e8f43143455f1dda8b
Nuradil Zhanadil uulu <nuradilzhanadiluulu21@gmail.com>
2024-02-09 23:10:44 -0600

    last commit

---
+++ server/routes/dalle.routes.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import * as dotenv from 'dotenv'
+import OpenAI from 'openai';
+
+dotenv.config();
+
+const router = express.Router();
+
+const openai = new OpenAI({apiKey: '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'})
+
+router.route('/').get((req, res) => {
+    res.status(200).json({message : 'Hello from DALL.E ROUTES'})
+})
+
+router.route('/').post(async(req, res) => {
+    try{
+        const { prompt } = req.body
+        
+        const response = await openai.images.generate({
+            prompt,
+            model: 'dall-e-2',
+            n: 1,
+            size: '1024x1024',
+            response_format: 'b64_json'
+        })
+     
+        console.log(response)
+    
+
+        const image = response.data.data[0].b64_json;
+
+        res.status(200).json({photo: image})
+
+    }catch(error){
+        console.log(error)
+        res.status(500).json({message: "Something went wrong"})
+    }
+})
+
+export default router
